ad84eaefe4eae8f51bb9f8cd2c7b8758528dc0d6
Emily Burger <eburger939@gmail.com>
2022-01-26 14:23:47 -0600

    created route to login when page local host is active

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const petRoutes = require('./pet-routes');
+// const petRoutes = require('./pet-routes');
 
 router.use('./user', userRoutes);
-router.use('./pet', petRoutes);
+// router.use('./pet', petRoutes);
 
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-page')
+
+
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 
 module.exports = router;
