723e2241cce9662a403b914eec31b0168aaef428
Tom M <T0930@protonmail.com>
2022-01-28 11:59:26 -0700

    fixed file link in index.js within API folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const petRoutes = require('./pet-routes');
 const vaxRoutes = require('./vax-routes');
-const medsRoutes = require('/meds-routes');
-const dxRoutes = require('/dx-routes');
+const medsRoutes = require('./meds-routes');
+const dxRoutes = require('./dx-routes');
 
 router.use('/user', userRoutes);
 router.use('/pet', petRoutes);
