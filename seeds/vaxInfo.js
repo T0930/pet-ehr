5778b4af98ecf46705645d548f27541c4a3a7906
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2022-01-28 14:29:55 -0700

    updated vaxinfo.js with dates corrected

--- seeds/vaxInfo.js
+++ seeds/vaxInfo.js
@@ -3,26 +3,26 @@ const { Vax } = require('../models');
 const vaxData = [
     {
         vax_name: 'parvovirus',
-        administered_date: '01/28/2022',
-        next_due: '01/28/2023',
+        administered_date: 'January 28, 2022',
+        next_due: 'January 28, 2023',
         pet_id: 1
     },
     {
         vax_name: 'rabies',
-        administered_date: '12/12/21',
-        next_due: '05/12/21',
+        administered_date: 'December 12, 2021',
+        next_due: 'May 12, 2022',
         pet_id: 1
     },
     {
         vax_name: 'adenovirus',
-        administered_date: '10/27/21',
-        next_due: '10/27/25',
+        administered_date: 'October 27, 2021',
+        next_due: 'October 27, 2025',
         pet_id: 1
     },
     {
         vax_name: 'hepatitis',
-        administered_date: '09/15/21',
-        next_due: '03/21/22',
+        administered_date: 'September 22, 2021',
+        next_due: 'March 22, 2022',
         pet_id: 1
     }
 ];
