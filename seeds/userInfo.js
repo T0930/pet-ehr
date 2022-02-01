0016a165db562f6c901f481e38e9f0077759dc23
Emily Burger <eburger939@gmail.com>
2022-01-31 20:09:33 -0600

    limtied seeds to one user and one pet

--- seeds/userInfo.js
+++ seeds/userInfo.js
@@ -1,26 +1,26 @@
 const { User } = require('../models');
 
 const userData = [
-    {
-        username: 'hedbear',
-        email: "bear@yahoo.com",
-        password: "12345678910"
-    },
-    {
-        username: 'sgarter',
-        email: "garter@yahoo.com",
-        password: "password"
-    },
+    // {
+    //     username: 'hedbear',
+    //     email: "bear@yahoo.com",
+    //     password: "12345678910"
+    // },
+    // {
+    //     username: 'sgarter',
+    //     email: "garter@yahoo.com",
+    //     password: "password"
+    // },
     {
         username: 'lillypad',
         email: "lilly@yahoo.com",
         password: "ilikepopcorn"
     },
-    {
-        username: 'ljohnson',
-        email: "ljohnson@yahoo.com",
-        password: "tillyriggs"
-    }
+    // {
+    //     username: 'ljohnson',
+    //     email: "ljohnson@yahoo.com",
+    //     password: "tillyriggs"
+    // }
 ];
 
 const seedUsers = () => User.bulkCreate(userData, {
